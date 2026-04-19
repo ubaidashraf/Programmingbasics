e4402e96e8988fdbc6bc0623120cc6b1a1b75e4c
Yameen Wani <yameenwani123@gmail.com>
2026-04-19 11:20:05 +0530

    program by yamee

--- c/evenodd_yamee.cpp
+++ c/evenodd_yamee.cpp
@@ -5,10 +5,11 @@ int num;
 printf("Enter a number: ");
 scanf("%d", &num);
 
-if (num%2==0)
+if (num%2==0){
 printf("Number is even", num);
-
-else 
+}
+else{ 
     printf("Number is odd", num);
-return 0;
+}
+    return 0;
 }
