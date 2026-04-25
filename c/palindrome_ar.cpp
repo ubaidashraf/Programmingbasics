5d8d5bae678610f6650b424542dba9406c15bafe
abdul rahman <aboodyisaac19@gmail.com>
2026-04-25 10:49:29 +0530

    rev of num

---
+++ c/palindrome_ar.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int num, original, reversed = 0, remainder;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    while (num != 0) {
+        remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num = num / 10;
+    }
+
+    if (original == reversed)
+        printf("Palindrome number\n");
+    else
+        printf("Not a palindrome\n");
+
+    return 0;
+}
