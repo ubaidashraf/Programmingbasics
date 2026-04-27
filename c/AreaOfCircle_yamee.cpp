4b2969f3fa038d577311492dcb82dc5735d1b58b
Yameen Wani <yameenwani123@gmail.com>
2026-04-27 14:34:34 +0530

    array

---
+++ c/AreaOfCircle_yamee.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int r,area;
+
+printf("Enter the valur of r:");
+scanf("%d",&r);
+
+area= 3.14*(r*r);
+
+printf("The area of circle is %d" , area);
+return 0;
+
+
+}
